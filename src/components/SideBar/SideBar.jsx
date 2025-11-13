313f820215378e03d688333cfa687d1a921e6acc
unknown <romanovska2807@gmail.com>
2025-11-13 09:05:33 +0200

    added sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,11 +1,18 @@
-export default function SideBar () {
+export default function SideBar ({stor}) {
+
+    const totalContacts = stor.length
     return (
         <aside className="container border-end shadow">
             <div className="row">
                 <div className="col-12">
                     <div className="contacts-labels">
-                        <div>
-                            
+                        <div className="fs-3 mb-5 mt-4 d-flex justify-content-between">
+                            <span>Всі контакти:</span><span>{totalContacts}</span>
+                        </div>
+                        <div className="list fs-5">
+                            <div className="d-flex justify-content-between mb-3">
+                                <div className="lav lab-success">Work</div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/pages/ContactList/ContactList.jsx
+++ src/pages/ContactList/ContactList.jsx
@@ -6,7 +6,7 @@ export default function ContactList({ stor, deleteContact }) {
         <div className="container rounded bg-white shadow-lg">
             <div className="row">
                 <div className="col-4">
-                    <SideBar />
+                    <SideBar stor={stor} />
                 </div>
                 <div className="col-8">
                     <ContactItem stor={stor} deleteContact={deleteContact} />
